6e79e11b2ade3b34811e716125df58ce2fda35b3
AtrainCode <56448516+AtrainCode@users.noreply.github.com>
2020-03-11 13:07:28 -0600

    Fixed("")

--- CombatController.java
+++ CombatController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import lib.ConsoleIO;
 import models.classes.Character;
+import models.classes.Warrior;
 import models.monsters.Monster;
 
 import java.util.Random;
@@ -10,85 +11,126 @@ public class CombatController {
     private int dmg;
     private int dmgTaken;
     private String combTxt;
-    private Character player;
-    private boolean isAlive;
+    private Character pl;
+    private boolean isAlive = true;
     private boolean enemyAlive;
     private int HPots, MPots;
     private int dung;
+
+//    Character ch = new Warrior("Jim",30,30,5,0,0,0,5,5,3);
+
+
     public int roll(int num, int sides) {
         int total = 0;
         for (int i = 0; i < num; i++) {
             Random ran = new Random();
-            System.out.println("On Roll: " + (i + 1) + " You got " + ran);
-            total += i;
+            int rand = ran.nextInt(sides)+1;
+            System.out.println("On Roll: " + (i+1) + " You got " + rand);
+            total += rand;
         }
         return total;
     }
 
-    public void encounter(Monster m){
-        int hp = m.getCurrentHP();
-        isAlive = player.getCurHp() > 0;
-        enemyAlive = m.getCurrentHP() > 0;
+    public void encounter(Character pl, Monster m){
+        this.pl = pl;
+        int gain = pl.getExp();
+        int xp = 10;
+        System.out.println("You've encountered a " + m.getName() + "\nit has " + m.getCurrentHP() + "HP!");
+
         do{
-            System.out.println("You've encounter a " + m.getName() + "\nit has " + m.getCurrentHP() + "HP!");
-            System.out.println("\nYou have " + player.getExp());
+        int hp = m.getCurrentHP();
+        isAlive = pl.getCurHp() > 0;
+        enemyAlive = m.getCurrentHP() > 0;
+
+//            System.out.println("\nYou have " + pl.getExp());
+            int initiate = roll(1, 20);
+            int dmgTaken = m.attack(roll(1,9 ));
+
             int input = ConsoleIO.promptForMenuSelection(FightMenu, false);
             switch (input){
                 case 1:
-                    dmgTaken = player.getCurHp() - m.attack(roll(2, 4));
-
-                    int initiate = player.roll(1, 20);
                     if (initiate < 8) {
                         dmg = 0;
-                        System.out.println("You've missed! The " + m.getName() + "took no damage!");
+                        System.out.println("You've missed! The " + m.getName() + " took no damage!");
                         System.out.println("The " + m.getName() + " struck you for " + dmgTaken + " Damage!");
+
                         if (dmgTaken < 1) {
                             System.out.println("The Monster missed!");
+
+                            isAlive = pl.getCurHp() > 0;
+                            enemyAlive = m.getCurrentHP() > 0;
                         }
-                    }
-                    if (initiate <= 18) {
-                        int hit = player.attack(roll(3, 6));
+                    } else {
+                    if (initiate >= 18) {
+                        int hit = pl.attack(roll(1, 1));
                         hit *= 2;
 //                        System.out.println("You hit a vital! The " + randSter() + " took " + hit + " Damage!");
-                        System.out.println(m.takeDamage(hp,hit));
-                    }
+                        System.out.println(m.takeDamage(hp, hit));
+                        System.out.println("The " + m.getName() + " struck you for " + dmgTaken + " Damage!");
+                        hp -= dmgTaken;
+                        isAlive = pl.getCurHp() > 0;
+                        enemyAlive = m.getCurrentHP() > 0;
+                        break;
+                    } else {
                     if (initiate > 8 && initiate < 18) {
-                        int hit = player.attack(roll(3, 6));
+                        int hit = pl.attack(roll(3, 6));
                         System.out.println("You hit the " + m.getName() + " for " + hit + " Damage!");
                         System.out.println(m.takeDamage(hp,hit));
+                        System.out.println("The " + m.getName() + " struck you for " + dmgTaken + " Damage!");
+                        hp -= dmgTaken;
+                        isAlive = pl.getCurHp() > 0;
+                        enemyAlive = m.getCurrentHP() > 0;
+                        break;
+                    } else {
+                        if (!isAlive) {
+                            charDied();
+                            System.out.println("You have been slain, It was a valiant effort, but Alas You've been vanquished");
+                            System.out.println("Along the way you've gained " + pl.getExp() + "xp!");
+                            break;
+                        }
                     }
-                    if (!isAlive){
-                        charDied();
-                        System.out.println("You have been slain, It was a valiant effort, but Alas You've been vanquished");
-                        System.out.println("Along the way you've gained " + player.getExp() + "xp!");
                     }
+                    }
+                    break;
                 case 2:
-                    if(player.getHPots() > 0 && player.getCurHp() < player.getHp()) {
-                        int  heal =  player.getCurHp() + player.getCurHp() + 10;
-                        player.setCurHp(heal);
-                        if(player.getCurHp()> player.getHp())
-                            player.setCurHp(player.getHp());
+                    if(pl.getHPots() > 0 && pl.getCurHp() < pl.getHp()) {
+                        int  heal =  pl.getCurHp() + pl.getCurHp() + 10;
+                        pl.setCurHp(heal);
+                        if(pl.getCurHp()> pl.getHp())
+                            pl.setCurHp(pl.getHp());
                         HPots--;
+
                     }else {
-                        System.out.println("You have no potions / You're already full!");
+                        System.out.println("You have no potions / You're already full!\n\n");
 
                     }
-
+                        break;
                 case 3:
-                    if(player.getMPots() > 0){
-                        int boost = player.attack(roll(3,6)+10);
+                    if(pl.getMPots() > 0){
+                        int boost = pl.attack(roll(3,6)+10);
                         System.out.println(m.takeDamage(hp,boost));
                         MPots--;
+                        System.out.println("The " + m.getName() + " struck you for " + dmgTaken + " Damage!");
+                        hp -= dmgTaken;
+                        isAlive = pl.getCurHp() > 0;
+                        enemyAlive = m.getCurrentHP() > 0;
+
 //                        System.out.println("Your boosted damage hit the " + randSter() + "for" + boost + "Damage!");
+                    } else if(pl.getMPots() < 1){
+                        System.out.println("You don't have any Mana potions!\n\n");
                     }
+                    break;
                 case 4:
-                    stats(player);
-                default:
-                    System.out.println("something happened, Try again!");
+                    stats();
+                    break;
+//                default:
+//                    System.out.println("something happened, Try again!");
             }
 
         }while(enemyAlive);
         System.out.println("\n\nYou beat the " + m.getName() + "!\n");
+         gain += xp;
+        System.out.println("You've gained " + gain + " exp!");
 
     }
     public static String[] FightMenu = {"Attack!","Drink Health Potion", "Magic Boost!", "Character Stats"};
@@ -100,8 +142,11 @@ public class CombatController {
 //    public Inventory inv(Inventory inv){
 //        return inv;
 //    }
-    public String stats(Character character){
-        return character.toString();
+    public void stats(){
+        System.out.println(pl.toString());
+//        System.out.println(pl.getName() + "HP: " + pl.getHp() + "\nXP: " + pl.getExp()
+//        + "Health Potions: " + pl.getHPots() + "Mana Potions: " + pl.getMPots()+pl.getClass());
+
     }
     public void charDied(){
         for(int i=0; i < 50; i++){
@@ -109,4 +154,3 @@ public class CombatController {
         }
     }
 }
-
